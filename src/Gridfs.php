0314daa9d4589cf01e9263e41340db64fdc1338a
Filipe Danielski Andrade <filipe@filipedanielski.com.br>
2017-12-11 18:16:42 -0200

    Added methods to download by filename and download original file.

--- src/Gridfs.php
+++ src/Gridfs.php
@@ -4,6 +4,11 @@ namespace Filipedanielski\Gridfs;
 
 trait Gridfs
 {
+    /*
+    |--------------------------------------------------------------------------
+    | Upload functions
+    |--------------------------------------------------------------------------
+    */
     public function upload($file, $metadata = [], $filename = null){
         $bucket = $this->connectToBucket();
 
@@ -16,14 +21,64 @@ trait Gridfs
         );
     }
 
-    public function download($id){
+    /*
+    |--------------------------------------------------------------------------
+    | Download functions
+    |--------------------------------------------------------------------------
+    */
+    public function download($id, $revision = null){
         $bucket = $this->connectToBucket();
 
         $stream = $bucket->openDownloadStream($id);
         $metadata = $bucket->getFileDocumentForStream($stream);
 
+        if($revision != null){
+            unset($stream);
+            $stream = $bucket->openDownloadStreamByName(
+                $metadata->filename, ['revision' => $revision]
+            );
+
+            unset($metadata);
+            $metadata = $bucket->getFileDocumentForStream($stream);
+        }
+
         $contents = stream_get_contents($stream);
 
+        return $this->prepareDownload($contents, $metadata);
+    }
+
+    public function downloadByFilename($filename, $revision = null){
+        $bucket = $this->connectToBucket();
+
+        if($revision != null){
+            $stream = $bucket->openDownloadStreamByName($filename);
+            $metadata = $bucket->getFileDocumentForStream($stream);
+        } else {
+            $stream = $bucket->openDownloadStreamByName(
+                $filename, ['revision' => $revision]
+            );
+            $metadata = $bucket->getFileDocumentForStream($stream);
+        }
+
+        $contents = stream_get_contents($stream);
+
+        return $this->prepareDownload($contents, $metadata);
+    }
+
+    public function downloadOriginal($filename){
+        $bucket = $this->connectToBucket();
+
+        $stream = $bucket->openDownloadStreamByName(
+            $filename, ['revision' => 0]
+        );
+        $metadata = $bucket->getFileDocumentForStream($stream);
+
+        $contents = stream_get_contents($stream);
+
+        return $this->prepareDownload($contents, $metadata);
+    }
+
+    public function prepareDownload($contents, $metadata){
         return response($contents)
             ->withHeaders([
                 'Content-Type'              => 'application/octet-stream',
@@ -38,13 +93,22 @@ trait Gridfs
         );
     }
 
+    /*
+    |--------------------------------------------------------------------------
+    | Search functions
+    |--------------------------------------------------------------------------
+    */
     public function findOne($filter = [], array $options = []){
         $bucket = $this->connectToBucket();
 
         return $bucket->findOne($filter, $options);
     }
 
-    
+    /*
+    |--------------------------------------------------------------------------
+    | Connection and configuration functions
+    |--------------------------------------------------------------------------
+    */
     public function connectToBucket(){
         return \Illuminate\Support\Facades\DB::connection(
             $this->connection ?: $this->app['config']['database.default']
--- tests/models/User.php
+++ tests/models/User.php
@@ -1,15 +1,9 @@
 <?php
 
 use Jenssegers\Mongodb\Eloquent\Model as Eloquent;
-use Illuminate\Auth\Authenticatable;
-use Illuminate\Auth\Passwords\CanResetPassword;
-use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
-use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 
-class User extends Eloquent implements AuthenticatableContract, CanResetPasswordContract
+class User extends Eloquent
 {
-    use Authenticatable, CanResetPassword;
-
     protected $connection = 'mongodb';
     protected static $unguarded = true;
 
