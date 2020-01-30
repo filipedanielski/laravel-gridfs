feee05c829f98531149c6a57b53bb3ddc40687ce
Filipe Danielski <filipe@filipedanielski.com.br>
2020-01-30 01:49:31 -0300

    Change testing bucket name

--- tests/config/database.php
+++ tests/config/database.php
@@ -7,7 +7,7 @@ return [
             'driver'   => 'mongodb',
             'host'     => '127.0.0.1',
             'port'     => '27017',
-            'database' => 'unittest',
+            'database' => 'testing',
         ],
     ],
 ];
